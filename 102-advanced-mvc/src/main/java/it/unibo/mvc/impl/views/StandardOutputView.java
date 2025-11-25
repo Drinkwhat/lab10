dff43b62d8fa12682de6419ffa301cf25faff434
Luca Bevilacqua <bevilacqua.luca05@gmail.com>
2025-11-25 11:05:45 +0100

    Add StandardOutputView implementation for DrawNumberView interface

---
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/impl/views/StandardOutputView.java
@@ -0,0 +1,37 @@
+package it.unibo.mvc.impl.views;
+
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawNumberViewObserver;
+import it.unibo.mvc.impl.DrawResult;
+
+public class StandardOutputView implements DrawNumberView {
+
+    @Override
+    public void setObserver(final DrawNumberViewObserver observer) {
+        /*
+         * This UI is output only.
+         */
+    }
+
+    @Override
+    public void start() {
+        /*
+         * PrintStreams are always ready.
+         */
+    }
+
+        @Override
+    public void numberIncorrect() {
+        System.out.println("You must enter a number");
+    }
+
+    @Override
+    public void result(final DrawResult res) {
+        System.out.println(res.getDescription());
+    }
+
+    @Override
+    public void displayError(final String message) {
+        System.out.println(message);
+    }
+}
